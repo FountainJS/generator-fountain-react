fd2a052bdf11fc0455545cc7a5fff562339e6596
Micaël <micael.mbagira@icloud.com>
2016-05-30 09:46:09 +0200

    Edit typings base url since upgrade

--- generators/techs/templates/src/app/footer.spec.tsx
+++ generators/techs/templates/src/app/footer.spec.tsx
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/main.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 /* tslint:disable:no-unused-variable */
 import * as React from 'react';
--- generators/todoMVC/inject/templates/src/app/components/Header.tsx
+++ generators/todoMVC/inject/templates/src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-/// <reference path="../../../typings/main.d.ts" />
+/// <reference path="../../../typings/index.d.ts" />
 
 interface IHeaderProps {
   addTodo: (text: string) => void;
