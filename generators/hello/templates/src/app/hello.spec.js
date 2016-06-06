a927c9f1f371e290221cc280efd2c2d3d1e71b35
Mehdy Dara <mdara@eleven-labs.com>
2016-06-06 16:11:58 +0200

    hello.spec.js written in ES6
    fix #41

--- generators/hello/templates/src/app/hello.spec.js
+++ generators/hello/templates/src/app/hello.spec.js
@@ -1,8 +1,8 @@
 /* eslint-env jasmine */
 <% if (modules === 'webpack') { -%>
-const React = require('react');
-const TestUtils = require('react/lib/ReactTestUtils');
-const Hello = require('./hello');
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+var Hello = require('./hello');
 <% } -%>
 <% if (modules === 'systemjs') { -%>
 import React from 'react';
@@ -10,13 +10,13 @@ import TestUtils from 'react/lib/ReactTestUtils';
 import Hello from './hello';
 <% } -%>
 <% if (modules === 'inject') { -%>
-const TestUtils = React.addons.TestUtils;
+var TestUtils = React.addons.TestUtils;
 <% } -%>
 
-describe('hello component', () => {
-  it('should render hello world', () => {
-    const hello = TestUtils.renderIntoDocument(<Hello/>);
-    const h1 = TestUtils.findRenderedDOMComponentWithTag(hello, 'h1');
+describe('hello component', function () {
+  it('should render hello world', function () {
+    var hello = TestUtils.renderIntoDocument(<Hello/>);
+    var h1 = TestUtils.findRenderedDOMComponentWithTag(hello, 'h1');
     expect(h1.textContent).toEqual('Hello world!');
   });
 });
